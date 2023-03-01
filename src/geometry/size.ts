f0fc7fa10b29d85b41ae26c7e815437d3e4e5d69
schlucht <web@jagolo.ch>
2023-03-01 05:59:26 +0000

    delete project

--- src/geometry/size.ts
+++
@@ -1,8 +0,0 @@
-export class Size {
-    public constructor(public Width: number, public Height: number) {      
-    }
-
-    public toString(): string {
-        return `Width: ${this.Width}, Height: ${this.Height}`;
-    }
-}
--- src/math/random.ts
+++
@@ -1,6 +0,0 @@
-
-function RandomDigit(initValue: number = 0): number {
-    return Math.floor(Math.random() * initValue);
-}
-
-export {RandomDigit}
